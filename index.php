8a6cb745a29f0ca7ed29de9fff03e083e98ea30f
yobhakejt <144523839+yobhakejt@users.noreply.github.com>
2023-10-21 15:12:41 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,49 +1,65 @@
 <?php
+// 문제 생성 함수
+function generateProblem() {
+    $operators = array('+', '*', '/'); // 사용할 연산자들
 
-// 랜덤으로 숫자 2개 선택
-$num1 = rand(1, 10);
-$num2 = rand(1, 10);
+    $operator = $operators[array_rand($operators)]; // 연산자 랜덤 선택
 
-// 랜덤으로 연산자 선택 (+, *, / 중에서)
-$operator = rand(1, 3);
-if ($operator == 1) {
-  // 더하기 문제
-  $question = "$num1 + $num2";
-  $answer = $num1 + $num2;
-} elseif ($operator == 2) {
-  // 곱하기 문제
-  $question = "$num1 * $num2";
-  $answer = $num1 * $num2;
-} else {
-  // 나누기 문제 (나머지가 없는 경우에만)
-  $num1 = $num1 * $num2; // num1을 num2의 배수로 만들어주기
-  $question = "$num1 ÷ $num2";
-  $answer = $num1 / $num2;
+    // 덧셈 문제 생성
+    if ($operator == '+') {
+        $num1 = rand(1, 100);
+        $num2 = rand(1, 100);
+        $result = $num1 + $num2;
+    }
+
+    // 곱셈 문제 생성
+    if ($operator == '*') {
+        $num1 = rand(1, 10);
+        $num2 = rand(1, 10);
+        $result = $num1 * $num2;
+    }
+
+    // 나눗셈 문제 생성
+    if ($operator == '/') {
+        $result = rand(1, 10);
+        $num2 = rand(1, 10);
+        $num1 = $result * $num2;
+    }
+
+    $problem = $num1 . ' ' . $operator . ' ' . $num2; // 문제 문자열 생성
+    return array('problem' => $problem, 'result' => $result); // 문제와 정답을 배열로 반환
 }
 
-// 문제 출력 후 답 입력 요청
-echo "문제: $question\n";
-$guess = readline("답: ");
-
-// 정답인 경우 새로운 문제 생성
-while ($guess == $answer) {
-  $num1 = rand(1, 10);
-  $num2 = rand(1, 10);
-  $operator = rand(1, 3);
-  if ($operator == 1) {
-    $question = "$num1 + $num2";
-    $answer = $num1 + $num2;
-  } elseif ($operator == 2) {
-    $question = "$num1 * $num2";
-    $answer = $num1 * $num2;
-  } else {
-    $num1 = $num1 * $num2;
-    $question = "$num1 ÷ $num2";
-    $answer = $num1 / $num2;
-  }
-  echo "정답입니다! 새로운 문제를 내겠습니다.\n";
-  echo "문제: $question\n";
-  $guess = readline("답: ");
+// 세션을 통해 현재 문제 정보 유지
+session_start();
+if (!isset($_SESSION['problem'])) {
+    $_SESSION['problem'] = generateProblem(); // 처음 문제 생성
 }
 
-echo "틀렸습니다. 정답은 $answer입니다.\n";
+// POST 요청 처리
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $userAnswer = $_POST['answer']; // 사용자가 입력한 답
+
+    // 정답 확인
+    if ($userAnswer == $_SESSION['problem']['result']) {
+        $_SESSION['problem'] = generateProblem(); // 다음 문제 생성
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>문제 풀기</title>
+</head>
+<body>
+    <h1>문제 풀기</h1>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <label for="answer">답:</label>
+        <input type="text" id="answer" name="answer">
+        <button type="submit">입력</button>
+    </form>
+
+    <h2>문제: <?php echo $_SESSION['problem']['problem']; ?></h2>
+</body>
+</html>

dff13202ccf16bc392d91d542deca77a0d20e649
yobhakejt <144523839+yobhakejt@users.noreply.github.com>
2023-10-21 15:15:29 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,65 +1,51 @@
-<?php
-// 문제 생성 함수
-function generateProblem() {
-    $operators = array('+', '*', '/'); // 사용할 연산자들
-
-    $operator = $operators[array_rand($operators)]; // 연산자 랜덤 선택
-
-    // 덧셈 문제 생성
-    if ($operator == '+') {
-        $num1 = rand(1, 100);
-        $num2 = rand(1, 100);
-        $result = $num1 + $num2;
-    }
-
-    // 곱셈 문제 생성
-    if ($operator == '*') {
-        $num1 = rand(1, 10);
-        $num2 = rand(1, 10);
-        $result = $num1 * $num2;
-    }
-
-    // 나눗셈 문제 생성
-    if ($operator == '/') {
-        $result = rand(1, 10);
-        $num2 = rand(1, 10);
-        $num1 = $result * $num2;
-    }
-
-    $problem = $num1 . ' ' . $operator . ' ' . $num2; // 문제 문자열 생성
-    return array('problem' => $problem, 'result' => $result); // 문제와 정답을 배열로 반환
-}
-
-// 세션을 통해 현재 문제 정보 유지
-session_start();
-if (!isset($_SESSION['problem'])) {
-    $_SESSION['problem'] = generateProblem(); // 처음 문제 생성
-}
-
-// POST 요청 처리
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $userAnswer = $_POST['answer']; // 사용자가 입력한 답
-
-    // 정답 확인
-    if ($userAnswer == $_SESSION['problem']['result']) {
-        $_SESSION['problem'] = generateProblem(); // 다음 문제 생성
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>문제 풀기</title>
-</head>
-<body>
-    <h1>문제 풀기</h1>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <label for="answer">답:</label>
-        <input type="text" id="answer" name="answer">
-        <button type="submit">입력</button>
-    </form>
+    <script>
+        function generateProblem() {
+            var num1 = Math.floor(Math.random() * 10) + 1; // 1부터 10까지의 랜덤한 숫자 생성
+            var num2 = Math.floor(Math.random() * 10) + 1;
 
-    <h2>문제: <?php echo $_SESSION['problem']['problem']; ?></h2>
+            var operatorIndex = Math.floor(Math.random() * 3); // 0, 1, 2 중에 하나의 랜덤한 숫자 생성
+            var operator;
+            var answer;
+
+            if (operatorIndex === 0) {
+                operator = '+';
+                answer = num1 + num2;
+            } else if (operatorIndex === 1) {
+                operator = '×';
+                answer = num1 * num2;
+            } else {
+                operator = '÷';
+                answer = num1 / num2;
+            }
+
+            document.getElementById('problem').textContent = num1 + ' ' + operator + ' ' + num2 + ' =';
+            document.getElementById('answer').value = ''; // 답 입력 박스 초기화
+            document.getElementById('answer').focus(); // 답 입력 박스에 포커스 설정
+
+            return answer;
+        }
+
+        function checkAnswer(correctAnswer) {
+            var userAnswer = parseInt(document.getElementById('answer').value);
+            
+            if (userAnswer === correctAnswer) {
+                alert('정답입니다!');
+                generateProblem();
+            } else {
+                alert('오답입니다. 다시 시도해주세요.');
+                // 이 부분에 IP 밴 처리하는 로직을 추가해야 합니다.
+            }
+        }
+    </script>
+</head>
+<body onload="generateProblem();">
+    <h1>문제 풀기</h1>
+    <div id="problem"></div>
+    <input type="number" id="answer">
+    <button onclick="checkAnswer()">입력</button>
 </body>
 </html>
